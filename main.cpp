422cd93f89faed3386ca612f7e71ffbfc38a4199
MrBrutalchiefs <1506340373@qq.com>
2021-09-01 13:02:50 +0800

    Add robin-map and tinyxml

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "tsl/robin_map.h"
 
-int main() {
+using namespace tsl;
+using namespace std;
 
+int main(int argc, char** argv) {
+    robin_map<int, string> map_test;
+    map_test[0] = "AAA";
+    map_test[1] = "BBB";
+    map_test[2] = "CCC";
+
+    cout << map_test[0] << map_test[1] << map_test[2] << endl;
 }
